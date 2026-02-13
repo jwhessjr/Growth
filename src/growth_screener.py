dbfbd5949a5079182359032823aac10743605e6c
Jim Hess <jhess2@gmail.com>
2026-02-13 15:39:52 -0500

    Added narative describing what I need to gather in data

--- src/growth_screener.py
+++ src/growth_screener.py
@@ -5,8 +5,13 @@ Growth Stock Screener
 from dataclasses import dataclass
 from datetime import date
 import sqlite3
-import hg_dcflib
+import hg_gs_lib
 import logging
 
 # Get the Risk Free Rate and the ERP
 # Add them together to create the hurdle rate
+
+# Get the financials for the last 40 quarters (10 years)
+# Revenue, EBIT
+# Debt, Equity
+# EPS, P/E ratio
